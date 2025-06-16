e7ba8451a339b086bc1fd470f8658559b7ed2627
Neha045 <sonpureneha@gmail.com>
2025-06-16 20:54:53 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/codepilot",
+  base: '',
   plugins: [react(), tailwindcss()],
 });
